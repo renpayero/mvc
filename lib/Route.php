856c7454011a49a49da49324875c7c2a77904d3b
Renzo <renzopayero@hotmail.com>
2024-04-24 13:42:52 -0300

    usando rutas con parametros

--- lib/Route.php
+++ lib/Route.php
@@ -23,18 +23,25 @@ class Route{
         $method = $_SERVER['REQUEST_METHOD'];
 
         foreach(self::$routes[$method] as $route => $callback){
+
             if (strpos($route, ':') !== false){
-                $route = preg_replace("#:([\W]+)#", "([^/]+)", $route);
+                $route = preg_replace('#:[a-zA-Z]+#', '([a-zA-Z]+)', $route);
             }
-            if (preg_match("#^$route$#", $uri)){
-                $callback();
+
+            if (preg_match("#^$route$#", $uri, $matches)){
+                $params = array_slice($matches, 1);
+
+                $response = $callback(...$params);
+
+                if (is_array($response) || is_object($response)){
+                    header('Content-Type: application/json');
+                    echo json_encode($response);
+                } else {
+                    echo $response;
+                }
                 return;
             }
-            // if ($route == $uri){
-            //     $callback();
 
-            //     return;
-            // }
         }
         echo '404 Not Found';
     }
--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,28 @@
 use lib\Route;
 
 Route::get('/', function(){
-    echo 'Hola desde la pagina principal';
+    return [
+        'title' => 'Inicio',
+        'message' => 'Hola desde la pagina de inicio'
+    ];
 });
 
 Route::get('/contact', function(){
-    echo 'Hola desde la pagina de contacto';
+    return 'Hola desde la pagina de contacto';
 });
 
 Route::get('/about', function(){
-    echo 'Hola desde la pagina de about';
+    return 'Hola desde la pagina de about';
 });
 
-Route::get('/user/:id', function(){
-    echo 'Hola desde la pagina de usuario';
+// el orden importa en las rutas porque se ejecutan de arriba hacia abajo
+Route::get('/users/perfil', function(){
+    return 'Hola desde la pagina de perfil';
 });
+
+Route::get('/users/:name', function($name){
+    return 'El usuario es ' . $name;
+});
+
 
 Route::dispatch();
